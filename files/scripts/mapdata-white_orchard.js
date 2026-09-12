2cfe9d7da843c99bf504c5d0f8440681736e7e26
Michael Singer <michael@a-singer.de>
2026-09-12 16:22:30 +0200

    Add one more honey comb to White Orchard

--- files/scripts/mapdata-white_orchard.js
+++ files/scripts/mapdata-white_orchard.js
@@ -467,6 +467,10 @@ window.mapdata_white_orchard = {
 		coords: [[-63.352,-112.324]],
 		label: $.t("sidebar.honeycomb"),
 		popup: $.t("honeycomb.desc")
+	},{
+		coords: [[-58.905,-135.176]],
+		label: $.t("sidebar.honeycomb"),
+		popup: $.t("honeycomb.desc")
 	}],
 	
 	//------------------------------------------------Innkeep------------------------------------------------
