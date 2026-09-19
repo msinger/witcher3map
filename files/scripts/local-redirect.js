0205c5e2b094f0a28e86a9f6929d6d86f0930407
Michael Singer <michael@a-singer.de>
2026-09-19 23:53:32 +0200

    Add missing anchor for previous commit

--- files/scripts/local-redirect.js
+++ files/scripts/local-redirect.js
@@ -5,5 +5,5 @@ let noRedirectParam = urlParams.get("noredirect") !== null;
 // use the same local storage of the browser for all maps. Settings will be shared between all
 // map views and backup/restore works as expected.
 if (window.location.protocol == "file:" && !noRedirectParam)
-	window.location.replace(window.topdir + "/index.html?map=" + mapInfos[0].ns);
+	window.location.replace(window.topdir + "/index.html?map=" + mapInfos[0].ns + window.location.hash);
 
